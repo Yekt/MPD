f28ed28eb9bf7c5bdc58bf80eeece90e9a69e2ab
Lukas <l-sauter@web.de>
2019-06-08 12:53:39 +0200

    Added settings toggle with esc key

--- What's Inside/Assets/Skripts/SceneChanger.cs
+++ What's Inside/Assets/Skripts/SceneChanger.cs
@@ -5,19 +5,40 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Linq;
 
-public class SceneChanger : MonoBehaviour{
+public class SceneChanger : MonoBehaviour {
 	
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.Escape)) {
+			AudioManager.Instance.Play("Click");
+			if(SceneManager.GetActiveScene().buildIndex == 1){
+				loadPreviousScene();
+			}
+			else loadScene(1);
+		}
+	}
 	
-    public void loadScene(int sceneNumber){		
-		PersistentData.Instance.sceneLog.Add(sceneNumber);
+    public void loadScene(int sceneNumber) {		
+		if(PersistentData.Instance.sceneLog[PersistentData.Instance.sceneLog.Count - 1] != sceneNumber){
+			PersistentData.Instance.sceneLog.Add(sceneNumber);
+		}
+		//print();
 		SceneManager.LoadScene(sceneNumber);
 	}
 	
-	public void loadPreviousScene(){
+	public void loadPreviousScene() {
 		if(PersistentData.Instance.sceneLog.Count > 1){
 			PersistentData.Instance.sceneLog.RemoveAt(PersistentData.Instance.sceneLog.Count - 1);
 			int scene = PersistentData.Instance.sceneLog[PersistentData.Instance.sceneLog.Count - 1];
+			//print();
 			SceneManager.LoadScene(scene);
 		}
+	}
+	
+	private void print(){
+		string str = "sceneLog:";
+		foreach (int i in PersistentData.Instance.sceneLog){
+			str += " " + i;
+		}
+		Debug.Log(str);	
 	}
 }
