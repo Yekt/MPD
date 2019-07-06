fe2fc29f25767bc68a6d3781a138320b6a9d8566
Lukas <l-sauter@web.de>
2019-07-07 01:20:46 +0200

    Fixed vacuum broken/fixed sprite toggle

---
+++ What's Inside/Assets/Scripts/MiniGames/Vacuum/toggleVacuum.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class toggleVacuum : MonoBehaviour
+{
+    public bool isFixedSprite;
+
+    void Start()
+    {
+        if (isFixedSprite)
+        {
+            if (PersistentData.Instance.vacuumFixed) this.gameObject.SetActive(true);
+            else this.gameObject.SetActive(false);
+        }
+        else
+        {
+            if (PersistentData.Instance.vacuumFixed) this.gameObject.SetActive(false);
+            else this.gameObject.SetActive(true);
+        }
+    }
+}
