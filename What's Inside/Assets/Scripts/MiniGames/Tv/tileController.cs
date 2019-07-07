6dfee34fd28b3d193fe02340e340e60125f562de
Lukas <l-sauter@web.de>
2019-07-07 22:26:41 +0200

    Added tv minigame

---
+++ What's Inside/Assets/Scripts/MiniGames/Tv/tileController.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+
+public class tileController : MonoBehaviour
+{
+    public tileHandler[,] tiles;
+    public GameObject controller;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioManager.Instance.Play("TVErstesBetreten1");
+
+        GameObject[] tmpTiles = new GameObject[controller.transform.childCount];
+        this.tiles = new tileHandler[4,8];
+        
+        
+        for (int i = 0; i < this.controller.transform.childCount; i++)
+        {
+            tmpTiles[i] = this.controller.transform.GetChild(i).transform.gameObject;
+        }
+
+
+        int counter = 0;
+        for (int y = 0; y < 4; y++)
+        {
+            
+            for (int x = 0; x < 8; x++)
+            {
+                this.tiles[y,x] = tmpTiles[counter].GetComponent(typeof(tileHandler)) as tileHandler;
+                counter++;
+            }
+        }
+        
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void checkForConnection()
+    {
+
+	    if (recursivCall(2, 0, Direction.RIGHT))
+	    {
+            PersistentData.Instance.tvFixed = true;
+            Inventory.Instance.activateGameItem("Antenne");
+            Inventory.Instance.deactivateItem("Fernseherkabel");            
+            AudioManager.Instance.Play("TVAbgeschlossen");
+        }
+        else
+        {
+            AudioManager.Instance.Play("VersuchsNochmal1");
+        }
+	    
+    }
+
+    
+	
+    //recursive Call to check if Game is connected correctly (Direction we entered is the direction we entered the current tile)
+    bool recursivCall(int y, int x, Direction directionWeGo)
+    {
+	    //Check if Starttile was found, if yes return true
+	    if (tiles[y, x].sortOfTile == SortOfTile.STARTTILE)
+	    {
+		    return true;
+	    }
+
+	    //Check if we left the playbox (which shouldnt be possible, but better check edge casses nonetheless), if yes, return false
+	    else if (y < 0 || y > 3 || x < 0 || x > 7)
+	    {
+		    Debug.Log("Out of Playbox. Something went terribly wrong, you sould have a look at this.");
+		    return false;
+	    }
+
+
+	    switch (directionWeGo)
+	    {
+		    case Direction.TOP:
+
+			    if (y - 1 < 0)
+			    {
+				    Debug.Log("Out of Playbox. Going to top. at Tile: X: " + x + "Y:" + y);
+				    return false;
+			    }
+			    
+			    if (tiles[y-1, x].sortOfTile == SortOfTile.STARTTILE)
+			    {
+				    return true;
+			    }
+
+
+			    if (tiles[y - 1, x].getConnections()[0] == Direction.BOT)
+			    {
+				    if (recursivCall(y - 1, x, tiles[y - 1, x].getConnections()[1])) return true;
+			    }
+			    else if (tiles[y - 1, x].getConnections()[1] == Direction.BOT)
+			    {
+				    if (recursivCall(y - 1, x, tiles[y - 1, x].getConnections()[0])) return true;
+			    }
+			    break;
+		    
+		    
+		    
+		    case Direction.RIGHT:
+			    if (x + 1 > 7)
+			    {
+				    Debug.Log("Out of Playbox. Coming from top. at Tile: X: " + x + "Y:" + y);
+				    return false;
+			    }
+
+			    if (tiles[y, x+1].sortOfTile == SortOfTile.STARTTILE)
+			    {
+				    return true;
+			    }
+
+			    
+			    if (tiles[y, x+1].getConnections()[0] == Direction.LEFT)
+			    {
+				    if (recursivCall(y, x+1, tiles[y, x+1].getConnections()[1])) return true;
+			    }
+			    else if (tiles[y, x+1].getConnections()[1] == Direction.LEFT)
+			    {
+				    if (recursivCall(y, x+1, tiles[y, x+1].getConnections()[0])) return true;
+			    }
+			    
+			    break;
+		    
+		    
+		    
+		    
+		    case Direction.BOT:
+			    if (y + 1 > 3)
+			    {
+				    Debug.Log("Out of Playbox. Coming from top. at Tile: X: " + x + "Y:" + y);
+				    return false;
+			    }
+
+			    
+			    if (tiles[y+1, x].sortOfTile == SortOfTile.STARTTILE)
+			    {
+				    return true;
+			    }
+
+			    
+			    if (tiles[y + 1, x].getConnections()[0] == Direction.TOP)
+			    {
+				    if (recursivCall(y + 1, x, tiles[y + 1, x].getConnections()[1])) return true;
+			    }
+			    else if (tiles[y + 1, x].getConnections()[1] == Direction.TOP)
+			    {
+				    if (recursivCall(y + 1, x, tiles[y + 1, x].getConnections()[0])) return true;
+			    }
+			    
+			    break;
+		    
+		    
+		    
+		    
+		    case Direction.LEFT:
+			    if (x - 1 < 0)
+			    {
+				    Debug.Log("Out of Playbox. Coming from top. at Tile: X: " + x + "Y:" + y);
+				    return false;
+			    }
+
+			    if (tiles[y, x-1].sortOfTile == SortOfTile.STARTTILE)
+			    {
+				    return true;
+			    }
+
+			    
+			    
+			    if (tiles[y, x-1].getConnections()[0] == Direction.RIGHT)
+			    {
+				    if (recursivCall(y, x-1, tiles[y, x-1].getConnections()[1])) return true;
+			    }
+			    else if (tiles[y, x-1].getConnections()[1] == Direction.RIGHT)
+			    {
+				    if (recursivCall(y, x-1, tiles[y, x-1].getConnections()[0])) return true;
+			    }
+
+
+
+			    break;
+
+
+	    }
+
+	    return false;
+    }
+}
---
+++ What's Inside/Assets/Scripts/MiniGames/Tv/tileHandler.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public enum TileRotation
+{
+    NOROTATION = 0, //0 Degrees
+    TILTEDRIGHT = 1, //90 Degrees
+    TURNEDARROUND = 2, //180 Degrees
+    TILTEDLEFT = 3 //270 Degrees
+}
+
+
+public enum SortOfTile
+{
+    STARTTILE,
+    ENDTILE,
+    STREIGHTTILE,
+    CURVETILE
+    
+}
+
+public enum Direction
+{
+    TOP = 0,
+    RIGHT = 1,
+    BOT = 2,
+    LEFT = 3
+}
+
+public class tileHandler : MonoBehaviour
+{
+    // Rotation of the Tile 
+    // 0 is no rotation
+    //        0
+    //     3  -  1
+    //        2
+    //
+    //public int rotationOfTheTile;
+
+    public TileRotation rotation; 
+    
+    // sortOfTile = StartTile;
+    //             x
+    //          x  -  x output
+    //             x
+    // 
+    // sortOfTile = EndTile;
+    //             x
+    //    input x  -  x 
+    //             x
+    //
+    // sortOfTile = StreightTile;
+    //             x
+    //    input x  -  x output
+    //             x
+    //
+    // sortOfTile = CurveTile;
+    //             x
+    //    input x  -  x 
+    //             x
+    //           output
+    //public int sortOfTile;
+    public SortOfTile sortOfTile;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+
+    
+    public Direction[] getConnections()
+    {
+        
+        Direction[] connections = new Direction[2];
+        
+        if (sortOfTile == SortOfTile.STREIGHTTILE)
+        {
+
+            switch (rotation)
+            {
+                case TileRotation.NOROTATION:
+                    connections[0] = Direction.RIGHT;
+                    connections[1] = Direction.LEFT;
+                    return connections;
+                
+                
+                case TileRotation.TILTEDRIGHT:
+                    connections[0] = Direction.TOP;
+                    connections[1] = Direction.BOT;
+                    return connections;
+                
+                
+                case TileRotation.TURNEDARROUND:
+                    connections[0] = Direction.RIGHT;
+                    connections[1] = Direction.LEFT;
+                    return connections;
+                
+                
+                case TileRotation.TILTEDLEFT:
+                    connections[0] = Direction.TOP;
+                    connections[1] = Direction.BOT;
+                    return connections;
+                
+            }
+            
+            
+            
+            
+        }else if (sortOfTile == SortOfTile.CURVETILE)
+        {
+            switch (rotation)
+            {
+                case TileRotation.NOROTATION:
+                    connections[0] = Direction.LEFT;
+                    connections[1] = Direction.BOT;
+                    return connections;
+                    
+                case TileRotation.TILTEDRIGHT:
+                    connections[0] = Direction.TOP;
+                    connections[1] = Direction.LEFT;
+                    return connections;
+                
+                case TileRotation.TURNEDARROUND:
+                    connections[0] = Direction.TOP;
+                    connections[1] = Direction.RIGHT;
+                    return connections;
+                
+                case TileRotation.TILTEDLEFT:
+                    connections[0] = Direction.RIGHT;
+                    connections[1] = Direction.BOT;
+                    return connections;
+            }
+            
+            
+            
+        }else if (sortOfTile == SortOfTile.ENDTILE)
+        {
+            switch (rotation)
+            {
+                case TileRotation.NOROTATION:
+                    connections[0] = Direction.LEFT;
+                    return connections;
+                case TileRotation.TILTEDRIGHT:
+                    connections[0] = Direction.TOP;
+                    return connections;
+                case TileRotation.TURNEDARROUND:
+                    connections[0] = Direction.RIGHT;
+                    return connections;
+                case TileRotation.TILTEDLEFT:
+                    connections[0] = Direction.BOT;
+                    return connections;
+                    
+            }
+        }
+        else
+        {
+            throw new Exception("getConnections should not be executed on Starttile. It should have finished the process. ");
+            
+        }
+        
+        return null;
+    }
+    
+    
+    public void rotateToPos(int rotationOfTheTile)
+    {
+        
+        RectTransform rectTransform = GetComponent<RectTransform>();
+        rectTransform.SetPositionAndRotation(rectTransform.position, default);
+        switch (this.rotation)
+        {
+            case TileRotation.NOROTATION:
+                Debug.Log("Rotation 0");
+                break;
+            case TileRotation.TILTEDRIGHT:
+                rectTransform.Rotate( new Vector3( 0, 0, -90 ) );
+                Debug.Log("Rotation -90");
+                break;
+            case TileRotation.TURNEDARROUND:
+                rectTransform.Rotate( new Vector3( 0, 0, -180 ) );
+                Debug.Log("Rotation -180");
+                break;
+            case TileRotation.TILTEDLEFT:
+                rectTransform.Rotate( new Vector3( 0, 0, -270 ) );
+                Debug.Log("Rotation -270");
+                break;
+        }
+    }
+
+
+    public void rotateRight()
+    {
+        if (this.sortOfTile == SortOfTile.STARTTILE || this.sortOfTile == SortOfTile.ENDTILE) return;
+        RectTransform rectTransform = GetComponent<RectTransform>();
+        rectTransform.Rotate(new Vector3(0,0, -90));
+        if (rotation == TileRotation.TILTEDLEFT)
+        {
+            rotation = 0;
+        }
+        else
+        {
+            rotation++;
+        }
+    }
+
+
+
+    }
