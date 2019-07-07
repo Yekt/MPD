a4561356fbe32770ee501704902f129abbf314e8
Lukas <l-sauter@web.de>
2019-07-07 23:21:22 +0200

    Added end

---
+++ What's Inside/Assets/Scripts/MiniGames/MinigameChecker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinigameChecker : MonoBehaviour
+{
+    public Sprite sprite;
+
+    void Start()
+    {
+        if (PersistentData.Instance.tvFixed &&
+            PersistentData.Instance.vacuumFixed &&
+            PersistentData.Instance.ovenFixed &&
+            PersistentData.Instance.radioFixed)
+        {
+            gameObject.GetComponent<Image>().sprite = sprite;
+            AudioManager.Instance.Play("SpielAbgeschlossen");
+        }
+    }
+}
