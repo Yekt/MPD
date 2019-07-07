f566478e0cf52746390000d1400261e20110e638
Lukas <l-sauter@web.de>
2019-07-07 22:53:30 +0200

    Implemented Item checker

---
+++ What's Inside/Assets/Scripts/MiniGames/ItemChecker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemChecker : MonoBehaviour
+{
+    public string[] items;
+    public int szene;
+    public SceneChangerAnimated sceneChanger;
+
+    public void enterGame()
+    {
+        bool flag = true;
+        foreach(string item in items)
+        {
+            if (!Inventory.Instance.wasFound("item")) flag = false;
+        }
+
+        if (flag) sceneChanger.loadScene(szene);
+        else Debug.Log("Couldn't find all Items");
+    }
+
+
+}
